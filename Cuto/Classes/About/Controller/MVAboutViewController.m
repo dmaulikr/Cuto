d2ffe9f238a529b608fa35f97b95bea5867a0960
macVirus <mr.zjx1992@163.com>
2017-01-14 10:28:04 +0800

    更新icon

--- Cuto/Classes/About/Controller/MVAboutViewController.m
+++ Cuto/Classes/About/Controller/MVAboutViewController.m
@@ -8,7 +8,7 @@
 
 #import "MVAboutViewController.h"
 #import "UIImageView+WebCache.h"
-
+#import <SafariServices/SafariServices.h>
 
 
 @interface MVAboutViewController ()
@@ -42,10 +42,10 @@
     vefView.alpha = 0.88;
     [imgView addSubview:vefView];
     NSLog(@"MMMMM");
-//    [self.view addSubview:imgView];
+    //    [self.view addSubview:imgView];
     self.tableView.backgroundView = imgView;
-//    [self.tableView.backgroundView addSubview:imgView];
-//        childVc.edgesForExtendedLayout = UIRectEdgeNone;
+    //    [self.tableView.backgroundView addSubview:imgView];
+    //        childVc.edgesForExtendedLayout = UIRectEdgeNone;
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
 }
@@ -64,6 +64,68 @@
     return  [super tableView:tableView cellForRowAtIndexPath:indexPath];
 }
 
+- (UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
 
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%ld",(long)indexPath.section);
+    // 0 -4
+    NSInteger num = [indexPath section];
+    switch (num) {
+        case 0:
+            [self donation];
+            break;
+        case 1:
+            [self showAFQ];
+            break;
+        case 2:
+            [self shreadApp];
+            break;
+        case 3:
+            [self goAppStore];
+            break;
+        case 4:
+            [self takeInfo];
+            break;
+        default:
+            break;
+    }
+}
+/// 捐赠
+-(void)donation{
+
+}
+
+
+/// AFQ
+-(void)showAFQ{
+    NSURL * url = [NSURL URLWithString:@"http://sspai.com/topic/cuto_ios_faq/"];
+    SFSafariViewController * sfvc = [[SFSafariViewController alloc] initWithURL:url];
+    [self presentViewController:sfvc animated:YES completion:nil];
+}
+
+
+/// 分享
+-(void)shreadApp{
+    UIPasteboard * psb = [UIPasteboard generalPasteboard];
+    [psb setString:@"123456"];
+
+    
+    UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:@[@"icon", [[NSBundle mainBundle] URLForResource:@"icon" withExtension:@"png"]] applicationActivities:nil];
+    [self.navigationController pushViewController:activity animated:YES];
+
+}
+
+/// 去评论
+-(void)goAppStore{
+    
+}
+
+/// 反馈
+-(void)takeInfo{
+
+}
 
 @end
